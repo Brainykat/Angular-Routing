3bbad022a4861023b9c8caab2eb50e5dd839cb61
Chris <eng.christhuku@gmail.com>
2022-03-30 16:05:31 +0300

    feat route param as observable

--- APM-Start/src/app/app.routing.module.ts
+++ APM-Start/src/app/app.routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { Route, RouterModule } from "@angular/router";
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 //import { Router } from '@angular/router';
+
+//Best Alternative
+export const Routes : Route[]= [
+        {path: 'welcome', component: WelcomeComponent},
+        {path: '', pathMatch : 'full', redirectTo: 'welcome'},
+        {path:'**',component:PageNotFoundComponent}
+      ]
 
 @NgModule({
     imports:[
