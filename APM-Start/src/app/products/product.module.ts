3a298c54fadfe8047c8b217d6680d9ea75128a5c
Chris <eng.christhuku@gmail.com>
2022-04-07 16:11:24 +0300

    feat child components

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -5,6 +5,8 @@ import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductResolver } from './product.resolver';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
@@ -13,13 +15,34 @@ import { SharedModule } from '../shared/shared.module';
     RouterModule.forChild([
       {path:'products', component:ProductListComponent},
       {path:'products/:id', component:ProductDetailComponent, resolve: {product:ProductResolver} },
-      {path:'products/:id/edit', component:ProductEditComponent, resolve: {product:ProductResolver}}
+      {
+        path:'products/:id/edit', 
+        component:ProductEditComponent, 
+        resolve: {product:ProductResolver},
+        children:[
+          {
+            path:'',
+            redirectTo:'info',
+            pathMatch:'full'
+          },
+          {
+            path:'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path:'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
+      }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
