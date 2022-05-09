e3415dc2734622d1e78b49ceeb0bab4754b69134
Chris <eng.christhuku@gmail.com>
2022-05-09 15:20:40 +0300

    feat auth guard for logged in user

--- APM-Start/src/app/messages/message.component.ts
+++ APM-Start/src/app/messages/message.component.ts
@@ -23,3 +23,4 @@ export class MessageComponent {
     this.messageService.isDisplayed=false;
   }
 }
+ 
--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -8,12 +8,14 @@ import { ProductResolver } from './product.resolver';
 import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { SharedModule } from '../shared/shared.module';
+import {AuthGuard} from '../user/auth.guard';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       { path:'products', 
+      canActivate:[AuthGuard],
         children:[
           {
             path:'',
